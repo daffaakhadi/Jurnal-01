9a7111b7ee7e21ce7dc6f77755cdaed8ce1899fb
daffaakhadi <akhadidaffa13@gmail.com>
2023-09-25 22:10:30 +0700

    initial commit

---
+++ Jurnal01.java
@@ -0,0 +1,8 @@
+public class Jurnal01{
+    public static void main(String[] args) {
+        System.out.println("Nama:Daffa Akhadi Yoga Perdana");
+        System.out.println("NIM:607062300050");
+        System.out.println("Kelas:D3IF-47-03");
+    }
+
+}
